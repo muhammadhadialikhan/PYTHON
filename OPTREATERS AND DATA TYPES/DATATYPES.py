0ea82178584d7fbda46ebf41743479ef316afd36
muhammadhadialikhan <hadiali010716@gmail.com>
2026-07-01 21:31:27 +0500

    python

---
+++ OPTREATERS AND DATA TYPES/DATATYPES.py
@@ -0,0 +1,15 @@
+name = "Hadi"
+age = 23
+isstudent = True
+weight = 10.5
+print("My name is ",name)
+print("My age is ",age)
+print("i Am a student? ",isstudent)
+print("My name is ",weight)
+print(type(name))
+print(type(age))
+print(type(isstudent))
+print(type(weight))
+print(int(weight))
+age = str (age)
+print(type(age))
