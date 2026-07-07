773b84095b69337ad3cb9f086e5dd57588e2e36f
muhammadhadialikhan <hadiali010716@gmail.com>
2026-07-07 21:27:32 +0500

    turtles

---
+++ Turtles/triangle.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.Screen().bgcolor("green")
+board = turtle.Turtle()
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
---
+++ Turtles/turtle1.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+
+sc = turtle.Screen()
+sc.setup(400,300)
+
+turtle.title("welcome to turtle window")
+turtle.done()
