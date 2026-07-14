6ad7e01176c7dec583038494f76579f2caf19664
muhammadhadialikhan <hadiali010716@gmail.com>
2026-07-14 21:25:19 +0500

    conditional statments

---
+++ conditional statments/even odd.py
@@ -0,0 +1,5 @@
+num = int(input("Enter an Number"))
+if num % 2== 0:
+    print("number is even")
+else:
+    print("number is even")
---
+++ conditional statments/greater.py
@@ -0,0 +1,9 @@
+number = int(input("Enter Your Number"))
+if number == 20:
+    print("number is equal to 20")
+elif number   <20:
+    print("number is smaller than 20")
+elif number   >20:
+    print("number is greater than 20")
+else:
+    print("Invalid number")
