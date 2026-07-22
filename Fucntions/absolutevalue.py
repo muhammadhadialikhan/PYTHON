1e83c152f733f1a006c06cc90b90d0ab052341ce
muhammadhadialikhan <hadiali010716@gmail.com>
2026-07-22 21:20:05 +0500

    Functions

---
+++ Fucntions/absolutevalue.py
@@ -0,0 +1,9 @@
+def absolute_value(num):
+    if num >= 0:
+        return num
+    else:
+        return -num
+print("absolute Value of 89 :", absolute_value(89))
+print("absolute Value of -189 :", absolute_value(-189))
+
+
